e15e9aff709b689374fbd7b01e64c0a89380efd0
kang2oon <kang2oon@gmail.com>
2022-07-24 17:57:10 +0900

    .,.,

--- the9sns/the9sns.js
+++ the9sns/the9sns.js
@@ -11,6 +11,10 @@ $(document).ready(function(){
     $('.modal_close').click(function(){
         $('.modal_wrap, #md_signin').hide();
     });
+
+    var cnt_num = $('.count_wrap div strong');
+    var cnt_num_com = cnt_num.toLocaleString('ko-KR');
+    cnt_num.text(cnt_num_com);
 });
 
 function md_signin(){
